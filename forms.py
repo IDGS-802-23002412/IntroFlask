b9fdc22353ef4bba3c07a8850e25220570f252e1
IDGS-802-23002412 <rdannabuendia@gmail.com>
2026-01-28 20:56:47 -0600

    Mensajes Flash

---
+++ forms.py
@@ -0,0 +1,28 @@
+from wtforms import Form
+from wtforms import StringField, IntegerField, PasswordField
+from wtforms import EmailField
+from wtforms import validators
+
+class UserForm(Form):
+    matricula = IntegerField('Matricula',[
+            validators.DataRequired(message='El campo es requerido'),
+            validators.NumberRange(min=100, max=1000, message='Ingrese un valor válido')
+    ])
+    nombre = StringField('Nombre',[
+            validators.DataRequired(message='El campo es requerido'),
+            validators.Length(min=3, max=10, message='Ingrese nombre válido')
+    ])
+
+    apaterno = StringField('Apellido Paterno',[
+            validators.DataRequired(message='El campo es requerido')
+    ])
+
+    amaterno = StringField('Apellido Materno',[
+            validators.DataRequired(message='El campo es requerido')
+    ])
+
+    correo = EmailField('Correo',[
+            validators.Email(message='Ingrese un correo válido')
+    ])
+
+
--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from flask import Flask, render_template, request
 from math import sqrt
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+import forms
 
 app = Flask(__name__)
+app.secret_key = 'Clave secreta'
+csrf = CSRFProtect()
 
 @app.route('/')
 def index():
     titulo="IDGS-802-Flask"
     lista={'Juan', 'Karla', 'Miguel', 'Ana'}
     return render_template('index.html', titulo=titulo, lista=lista)
+
+@app.route("/usuarios" , methods=["GET", "POST"])
+def usuarios():
+    mat = 0
+    nom = ''
+    apa = ''
+    ama = ''
+    email = ''
+    usuarios_class = forms.UserForm(request.form)
+    
+    if request.method == 'POST' and usuarios_class.validate():
+        mat = usuarios_class.matricula.data 
+        nom = usuarios_class.nombre.data 
+        apa = usuarios_class.apaterno.data
+        ama = usuarios_class.amaterno.data 
+        email = usuarios_class.correo.data
+
+        mensaje = 'Bienvenido {}'.format(nom)
+        flash(mensaje)
+
+    return render_template("usuarios.html", form=usuarios_class, 
+                           mat=mat, nom=nom, apa=apa,ama=ama, email=email)
 
 @app.route ("/formularios")
 def formularios():
@@ -102,5 +129,44 @@ def distancia():
 
     return render_template("distancia.html",x1=x1, y1=y1, x2=x2, y2=y2, res=res)
 
+@app.route("/cinepolis", methods=["GET", "POST"])
+def cinepolis():
+    precioBoleta = 12
+    total = 0
+    mensaje = ""
+
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+        cantidadCompradores = int(request.form.get("cantidadCompradores"))
+        cantidadBoletas = int(request.form.get("cantidadBoletas"))
+        tarjetaCineco = request.form.get("opcion")
+
+        maxBoletos = cantidadCompradores * 7
+
+        if cantidadBoletas > maxBoletos:
+            mensaje = (
+                f"{nombre}"
+                f"No puedes comprar más de "
+                f"{maxBoletos} boletas por cantidad de compradores "
+            )   
+            return render_template("cinepolis.html", mensaje=mensaje)
+
+        subtotal = cantidadBoletas * precioBoleta
+
+        if cantidadBoletas > 5:
+            subtotal *= 0.85
+        elif cantidadBoletas >= 3:
+            subtotal *= 0.90
+
+        if tarjetaCineco == "Si":
+            total = subtotal * 0.90
+        else:
+            total = subtotal
+
+        return render_template("cinepolis.html",total=total,mensaje=mensaje)
+
+    return render_template("cinepolis.html")
+
 if __name__ == '__main__':
+    csrf.init_app(app)
     app.run(debug=True)
